c3640ca7d081743ef7d2e49de04e4259e4661bd3
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-04-08 19:32:26 +0530

    files updated

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="left-h">
+        <Header />
+        {/* the best ad */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>the best fitness club in the town</span>
+        </div>
+        {/* hero-head */}
+        <div className="hero-text">
+          <div>
+            <span className="outline-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+          <span>Ideal Body</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="right-h"> right side</div>
+    </div>
+  );
+};
+
+export default Hero;
