60a0f47589b2af5364d837237cb15a78a460ffe6
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-04-18 16:52:01 +0530

    files updated

---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Programs.css";
+
+const Programs = () => {
+  return (
+    <div className="Programs" id="programs">
+      <div className="programs-header">
+        <span>Explore Our</span>
+        <span> Programs</span>
+        <span> To Shape Yourself</span>
+      </div>
+    </div>
+  );
+};
+
+export default Programs;
