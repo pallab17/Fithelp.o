824c7c7fc5f2baf7590739c68ee67881060d537a
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-04-18 17:15:43 +0530

    files updated

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import "./Programs.css";
+import { programsData } from "../../data/programsData";
+import RightArrow from "../../assets/rightArrow.png";
 
 const Programs = () => {
   return (
     <div className="Programs" id="programs">
       <div className="programs-header">
-        <span>Explore Our</span>
+        <span className="outline-text">Explore Our</span>
         <span> Programs</span>
-        <span> To Shape Yourself</span>
+        <span className="outline-text"> To Shape Yourself</span>
       </div>
-      
+
+      <div className="program-categories">
+        {programsData.map((program) => (
+          <div className="category">
+            {program.image}
+            <span> {program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span> Join Now</span>
+              <img src={RightArrow} alt="" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
