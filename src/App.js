7eb3d3a60c6194ee5888a4cabe8bfeb8d2215f9f
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-04-20 22:46:32 +0530

    files updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
 import Testimonials from './components/Testimonials/Testimonials';
-import Join from "./components/Join";
+import Join from "./components/Join/Join";
 function App() {
   return (
     <div className="App">
