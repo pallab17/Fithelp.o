f3243e9a4737f64143954c28238e6cdeaaaff7d9
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-04-20 14:37:28 +0530

    files updated

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Testimonials.css";
+import {testimonialsData} from "../../data/testimonialsData";
 
 const Testimonials = () => {
+    const[selected, setSelected]=useState(0);
+    const tlengtyh = testimonialsData.length;
+
+
   return (
     <div className="Testimonials">
       <div className="left-t">
         <span>TESTIMONIALS</span>
         <span className="outline-text">WHAT THEY</span>
         <span>SAY ABOUT US</span>
+        <span>
+            {testimonialsData[selected].review}
+        </span>
+        <span>
+            <span>
+                {testimonialsData[selected].name}
+            </span> {" "}
+            - {testimonialsData[selected].status}
+        </span>
       </div>
       <div className="right-t"></div>
     </div>
