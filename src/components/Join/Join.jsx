837738b80d7353a599e3dbcdc0ea760f622d1f05
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-04-20 22:58:20 +0530

    files updated

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -8,14 +8,17 @@ const Join = () => {
       <hr />
         <div>
           <span className="outline-text">READY TO</span>
-          <span> LEVEL UP</span>
+          <span>LEVEL UP</span>
         </div>
         <div>
           <span>YOUR BODY</span>
-          <span className="outline-text"> WITH US</span>
+          <span className="outline-text">WITH US</span>
         </div>
       </div>
-      <div className="right-j"></div>
+      <div className="right-j">
+
+        
+      </div>
     </div>
   );
 };
