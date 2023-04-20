ba09bf936295fe4f58315f22e6318652c9c0efe3
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-04-20 14:18:32 +0530

    files updated

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Testimonials.css";
+
+const Testimonials = () => {
+  return (
+    <div className="Testimonials">
+      <div className="left-t">
+        <span>TESTIMONIALS</span>
+        <span>WHAT THEY</span>
+        <span>SAY ABOUT US</span>
+      </div>
+      <div className="right-t"></div>
+    </div>
+  );
+};
+
+export default Testimonials;
